85ac466beac629e8392b72e07c4994288cdd1c46
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-07-26 12:16:48 +0200

    upDate

---
+++ src/main/java/Epicodeitjava4D3/progetto/D3/ProgettoD3Application.java
@@ -0,0 +1,13 @@
+package Epicodeitjava4D3.progetto.D3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoD3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoD3Application.class, args);
+	}
+
+}
---
+++ src/test/java/Epicodeitjava4D3/progetto/D3/ProgettoD3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Epicodeitjava4D3.progetto.D3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoD3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
